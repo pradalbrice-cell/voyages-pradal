e23fcd232023ab8f20dffee222ef340222e35019
pradalbrice-cell <pradal.brice@gmail.com>
2026-09-13 19:20:42 +0200

    Set Mon Cycle V3 version

--- MonCycleApp/app/build.gradle.kts
+++ MonCycleApp/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.moncycle.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "3.0"
     }
 
     compileOptions {
