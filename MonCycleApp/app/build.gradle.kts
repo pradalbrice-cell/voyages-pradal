518bb57ac9f5bb8a82c4d72c11bb941bc58346d2
pradalbrice-cell <pradal.brice@gmail.com>
2026-09-14 19:30:57 +0200

    Bump Mon Cycle to V3.4

--- MonCycleApp/app/build.gradle.kts
+++ MonCycleApp/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "fr.moncycle.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "3.3"
+        versionCode = 8
+        versionName = "3.4"
     }
 
     compileOptions {
